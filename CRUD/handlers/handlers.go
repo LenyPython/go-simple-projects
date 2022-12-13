ad68aa3f5fe4e24ad1171dcff16644d9c1f575c8
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2022-12-13 23:41:37 +0100

    Adding save handler

--- CRUD/handlers/handlers.go
+++ CRUD/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"CRUD/strucs"
 	"encoding/json"
 	"net/http"
-	"CRUD/strucs"
+  "math/rand"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -24,13 +27,24 @@ func GetMovie(res http.ResponseWriter, req *http.Request) {
   }
   json.NewEncoder(res).Encode(movie)
 }
+func SaveMovie(res http.ResponseWriter, req *http.Request){
+  res.Header().Set("Content-type", "application/json")
+  var movie strucs.Movie
+  _ = json.NewDecoder(req.Body).Decode(&movie)
+  movie.Id = strconv.Itoa(rand.Intn(100000))
+  moviesDB = append(moviesDB, movie)
+  json.NewEncoder(res).Encode(movie)
+}
 func DeleteMovie(res http.ResponseWriter, req *http.Request){
   res.Header().Set("Content-type","application/json")
   params := mux.Vars(req)
+  var deleted strucs.Movie
   for i, item := range moviesDB {
     if item.Id == params["id"]{
+      deleted = item
       moviesDB = append(moviesDB[:i],moviesDB[i+1:]...)
       break
     }
   }
+  json.NewEncoder(res).Encode(deleted)
 }
