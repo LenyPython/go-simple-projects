aa6164a724b13377dbf47b10e4fed2f4f65b8745
Piotr Lenartowicz <piotr.lenartowicz@yahoo.com>
2023-01-05 21:35:14 +0100

    changes

--- adv_proj/pkg/routes/bookstore-routes.go
+++ adv_proj/pkg/routes/bookstore-routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(r *mux.Router){
-  r.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-  r.HandleFunc("/book/", controllers.GetAllBooks).Methods("GET")
+  r.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+  r.HandleFunc("/books", controllers.GetAllBooks).Methods("GET")
   r.HandleFunc("/book/{id}", controllers.GetBook).Methods("GET")
   r.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
   r.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
